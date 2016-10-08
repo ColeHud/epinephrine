c62108a869b3481847b38d1575090bbdfad18509
ColeHud <cmhudson11@gmail.com>
2016-10-08 19:16:48 -0400

    Set up structure for app

---
+++ Epinephrine/Epinephrine/BarcodeReader.swift
@@ -0,0 +1,134 @@
+//
+//  ViewController.swift
+//  Epinephrine
+//
+//  Created by Cole on 10/8/16.
+//  Copyright © 2016 MHacks. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class BarcodeReader: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var session: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create a session object.
+        session = AVCaptureSession()
+        
+        // Set the captureDevice.
+        let videoCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        // Create input object.
+        let videoInput: AVCaptureDeviceInput?
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        // Add input to the session.
+        if (session.canAddInput(videoInput)) {
+            session.addInput(videoInput)
+        } else {
+            print("Can't Scan")
+        }
+        
+        // Create output object.
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        // Add output to the session.
+        if (session.canAddOutput(metadataOutput)) {
+            session.addOutput(metadataOutput)
+            
+            // Send captured data to the delegate object via a serial queue.
+            metadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            
+            // Set barcode type for which to scan: EAN-13.
+            metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeEAN13Code]
+            
+        } else {
+            print("Can't scan")
+        }
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        // Get the first object from the metadataObjects array.
+        if let barcodeData = metadataObjects.first {
+            // Turn it into machine readable code
+            let barcodeReadable = barcodeData as? AVMetadataMachineReadableCodeObject;
+            if let readableCode = barcodeReadable {
+                // Send the barcode as a string to barcodeDetected()
+                barcodeDetected(readableCode.stringValue);
+            }
+            
+            // Vibrate the device to give the user some feedback.
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
+            // Avoid a very buzzy device.
+            session.stopRunning()
+        }
+    }
+    
+    func barcodeDetected(code: String) {
+        
+        // Let the user know we've found something.
+        let alert = UIAlertController(title: "Found a Barcode!", message: code, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Search", style: UIAlertActionStyle.Destructive, handler: { action in
+            
+            // Remove the spaces.
+            let trimmedCode = code.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+            
+            // EAN or UPC?
+            // Check for added "0" at beginning of code.
+            
+            let trimmedCodeString = "\(trimmedCode)"
+            var trimmedCodeNoZero: String
+            
+            if trimmedCodeString.hasPrefix("0") && trimmedCodeString.characters.count > 1 {
+                trimmedCodeNoZero = String(trimmedCodeString.characters.dropFirst())
+                
+                // Send the doctored UPC to DataService.searchAPI()
+                //trimmedCodeNoZero
+            } else {
+                
+                // Send the doctored EAN to DataService.searchAPI()
+                //trimmedCodeString
+            }
+            
+            self.navigationController?.popViewControllerAnimated(true)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        if (session?.running == false) {
+            session.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (session?.running == true) {
+            session.stopRunning()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
+
